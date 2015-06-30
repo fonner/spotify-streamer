7e5c65908a21ec53e1c04ef006a81adef553b903
unknown <benjamin.fonner@ARSNTASH3631.arrsisnet.com>
2015-06-30 14:57:22 -0400

    Competed Search Functionality
    Cleaned up UI in Artist Search
    Started Playlist UI mockups

--- app/src/main/java/com/hitechren/spotifystreamer/SearchFragment.java
+++ app/src/main/java/com/hitechren/spotifystreamer/SearchFragment.java
@@ -1,26 +1,42 @@
 package com.hitechren.spotifystreamer;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.hitechren.spotifystreamer.data.ArtistAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 public class SearchFragment extends Fragment {
 
-    ArrayAdapter<String> mForecastAdapter;
-    private static final String LOG_TAG = SearchFragment.class.getName();
+    ArrayAdapter<Artist> mArtistAdapter;
+    private static final String LOG_TAG = SearchFragment.class.getSimpleName();
 
 
     public SearchFragment() {
         // Required empty public constructor
+    }
+
+    public void executeSearch(String probe){
+        FetchArtistsTask artistsTask = new FetchArtistsTask();
+        artistsTask.execute(probe);
     }
 
     @Override
@@ -31,34 +47,61 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        // Create some dummy data for the ListView.  Here's a sample weekly forecast
-        Log.v(LOG_TAG, "In CreateView");
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
-
-        // Now that we have some dummy forecast data, create an ArrayAdapter.
-        // The ArrayAdapter will take data from a source (like our dummy forecast) and
-        // use it to populate the ListView it's attached to.
-        mForecastAdapter =
-                new ArrayAdapter<String>(
-                        getActivity(), // The current context (this activity)
-                        R.layout.list_item_artist, // The name of the layout ID.
-                        R.id.textview_artist, // The ID of the textview to populate.
-                        weekForecast);
 
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+
+        EditText searchBox = (EditText)rootView.findViewById(R.id.edittext_search_artist);
+        searchBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    executeSearch(v.getText().toString());
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+
+        Log.v(LOG_TAG, "In CreateView");
+        Artist[] data = new Artist[7];
+        for (int i=0; i<7; i++){
+            Artist newArt = new Artist();
+            newArt.name = "Atrist " + Integer.toString(i);
+            data[i] = newArt;
+        }
+        ArrayList<Artist> initData = new ArrayList<>(Arrays.asList(data));
+
+        mArtistAdapter =
+                new ArtistAdapter(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_artist, // The name of the layout ID.
+                        new ArrayList<Artist>());
+
         ListView listView = (ListView) rootView.findViewById(R.id.listview_artists);
-        listView.setAdapter(mForecastAdapter);
+        listView.setAdapter(mArtistAdapter);
+        executeSearch("Elvis");
         return rootView;
+    }
+    public class FetchArtistsTask extends AsyncTask<String, Void, ArtistsPager>{
+
+        @Override
+        protected ArtistsPager doInBackground(String... params) {
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
+
+            return spotify.searchArtists(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(ArtistsPager result) {
+            if(result != null){
+                mArtistAdapter.clear();
+                for(int i =0; i < result.artists.items.size(); i++){
+                    mArtistAdapter.add(result.artists.items.get(i));
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/hitechren/spotifystreamer/data/ArtistAdapter.java
+++ app/src/main/java/com/hitechren/spotifystreamer/data/ArtistAdapter.java
@@ -1,22 +1,79 @@
 package com.hitechren.spotifystreamer.data;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hitechren.spotifystreamer.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Image;
 
 /**
  * Created by benjamin.fonner on 6/29/2015.
  */
 public class ArtistAdapter  extends ArrayAdapter<Artist>{
-    public ArtistAdapter(Context context, int resource, Artist[] objects) {
+    Context context;
+    int layoutResourceId;
+    ArrayList<Artist> data = null;
+
+    public ArtistAdapter(Context context, int resource, ArrayList<Artist> objects) {
         super(context, resource, objects);
+        this.layoutResourceId = resource;
+        this.context = context;
+        this.data = objects;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+
+        ArtisHolder placeHolder = null;
+
+        if(convertView == null){
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            convertView = inflater.inflate(layoutResourceId, parent, false);
+
+            placeHolder = new ArtisHolder();
+            placeHolder.imageview_artist = (ImageView)convertView.findViewById(R.id.imageview_artist);
+            placeHolder.textview_artist = (TextView)convertView.findViewById(R.id.textview_artist);
+
+            convertView.setTag(placeHolder);
+
+        } else {
+            placeHolder = (ArtisHolder)convertView.getTag();
+        }
+
+        Artist rowArtist = data.get(position);
+        if(rowArtist.images.size() != 0){
+            int sHeight = 0; //Smallest known height
+            int imageID = 0;
+            for(Image img : rowArtist.images){
+                if (sHeight == 0 || (img.height < sHeight && img.height >100)){
+                    sHeight = img.height;
+                }
+            }
+            Picasso.with(context).load(rowArtist.images.get(imageID).url)
+                    .into(placeHolder.imageview_artist);
+            placeHolder.imageview_artist.setBackgroundColor(0x00000000);
+        } else {
+            placeHolder.imageview_artist.setBackgroundColor(0xFFFF0000);
+            placeHolder.imageview_artist.setImageResource(android.R.color.transparent);
+        }
+        placeHolder.textview_artist.setText(rowArtist.name);
+
+        return convertView;
+    }
+
+    static class ArtisHolder{
+        ImageView imageview_artist;
+        TextView textview_artist;
     }
 }
