e94acb76a5dae28330b13ee6c2de0b6e93a531bc
Resol <bftech.sysadmin@gmail.com>
2015-07-12 16:00:32 -0400

    Removed extraneous logging statement
    Left over verbose log item in SearchFragment's onCreateView, line removed

--- app/src/main/java/com/hitechren/spotifystreamer/SearchFragment.java
+++ app/src/main/java/com/hitechren/spotifystreamer/SearchFragment.java
@@ -65,8 +65,7 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.v(LOG_TAG, "In onCreateView");
-
+       
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
 
         EditText searchBox = (EditText)rootView.findViewById(R.id.edittext_search_artist);
